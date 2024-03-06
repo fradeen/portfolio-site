b92155071a9bb5b0a9bc3683b2eb9cbc3485c395
Mohd Fardeen <frad.fardeen@gmail.com>
2024-03-06 09:52:52 +0000

    switch to mardown rendering for about page, update User schema, fetch data from DB for home and about page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image"
 import Link from "next/link";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+
+  const userInfo = await prisma.user.findFirstOrThrow()
   return (
     <div className='grow flex flex-col sm:flex-row-reverse h-full w-full border-2 p-2 border-gray-500'>
       <div className='relative my-1 sm:my-0 sm:mx-1 w-fit h-fit rounded-lg overflow-hidden self-center'>
         <Image
-          src='https://images.pexels.com/photos/574070/pexels-photo-574070.jpeg?cs=srgb&dl=pexels-lukas-574070.jpg&fm=jpg&w=6144&h=4069&_gl=1*51sk5i*_ga*ODY2MjY3NTk1LjE3MDg1MTEwODQ.*_ga_8JE65Q40S6*MTcwODc3MjMzMC4yLjEuMTcwODc3MjYyMi4wLjAuMA..'
+          src={userInfo?.imgURL}
           width={1920}
           height={1080}
-          alt="hero image"
+          alt="Author image"
         />
       </div>
       <div className='grow my-1 sm:my-0 sm:mx-1 w-full sm:self-center flex flex-col'>
-        <p className='text-7xl'>Mohd Fardeen</p>
-        <div>2</div>
+        <p className='text-7xl'>{userInfo.name}</p>
+        <div>{userInfo.intro}</div>
         <Link href='/about' className='bg-green-500 text-white w-fit p-1 rounded-lg self-center sm:self-start'>Know more.</Link>
       </div>
     </div>
--- src/components/renderMarkdown/renderMarkdown.tsx
+++ src/components/renderMarkdown/renderMarkdown.tsx
@@ -1,115 +1,74 @@
 'use client'
 import React from 'react'
 import { useThemeContext } from '@/contexts/themeContext';
-import Markdown from 'react-markdown'
+import Image from 'next/image';
+import Markdown, { Components } from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
+import remarkUnwrapImages from 'remark-unwrap-images'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark as darkTheme, oneLight as lightTheme } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-export default function RenderMarkdown() {
-    const value = `
-<div class='text-5xl'>test</div>
+export default function RenderMarkdown({ markdown }: { markdown: string }) {
 
-~~continuing with the article~~
- Just a link: www.nasa.gov.
-[Link text](https://google.com)
-hello 123
-![alt text for screen readers](https://images.pexels.com/photos/574070/pexels-photo-574070.jpeg?cs=srgb&dl=pexels-lukas-574070.jpg&fm=jpg&w=6144&h=4069&_gl=1*51sk5i*_ga*ODY2MjY3NTk1LjE3MDg1MTEwODQ.*_ga_8JE65Q40S6*MTcwODc3MjMzMC4yLjEuMTcwODc3MjYyMi4wLjAuMA.. "Text to show on mouseover" )
-
-<img src="https://images.pexels.com/photos/574070/pexels-photo-574070.jpeg?cs=srgb&dl=pexels-lukas-574070.jpg&fm=jpg&w=6144&h=4069&_gl=1*51sk5i*_ga*ODY2MjY3NTk1LjE3MDg1MTEwODQ.*_ga_8JE65Q40S6*MTcwODc3MjMzMC4yLjEuMTcwODc3MjYyMi4wLjAuMA" width="40%" />
-
-\`\`\`c
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
-\`\`\`
-<div class='text-2xl'>test</div>
-
-<Image src="https://images.pexels.com/photos/574070/pexels-photo-574070.jpeg?cs=srgb&dl=pexels-lukas-574070.jpg&fm=jpg&w=6144&h=4069&_gl=1*51sk5i*_ga*ODY2MjY3NTk1LjE3MDg1MTEwODQ.*_ga_8JE65Q40S6*MTcwODc3MjMzMC4yLjEuMTcwODc3MjYyMi4wLjAuMA" width=500 height=500 />
-
-<p align="center">
-  <img src="https://github.com/waldyr/Sublime-Installer/blob/master/sublime_text.png?raw=true" alt="Sublime's custom image"/>
-</p>
-
-
-
-## Table
-
-| a | b  |  c |  d  |
-| - | - | - | - |
-
-
-
-
-# GFM
-
-## Autolink literals
-
-www.example.com, https://example.com, and contact@example.com.
-
-## Footnote
-
-A note[^1]
-
-[^1]: Big note.
-
-## Strikethrough
-
-~one~ or ~~two~~ tildes.
-
-## Table
-
-| a | b  |  c |  d  |
-| - | :- | -: | :-: |
-
-## Tasklist
-
-* [ ] to do
-* [x] done
-`
-    const test = '# Hi, *Pluto*!'
     const { theme } = useThemeContext()
+    const components: Partial<Components> = {
+        code(props) {
+            const { children, className } = props
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+                <div className='flex flex-col'>
+                    <SyntaxHighlighter
+                        className='w-fit self-center'
+                        wrapLongLines
+                        PreTag="div"
+                        language={match[1]}
+                        style={theme === 'dark' ? darkTheme : lightTheme}
+                    >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+                </div>
+            ) : (
+                <div className='flex flex-col'>
+                    <code className={`${className} w-fit self-center`}>
+                        {children}
+                    </code>
+                </div>
+            )
+        },
+        img(props) {
+            return props.src ? (
+                <div className={` ${props.title?.search('@float') != -1 ? 'md:float-right' : ''} rounded-lg overflow-hidden m-5 md:my-0 w-fit`}>
+                    <Image
+                        src={props.src}
+                        title={props.title?.replace('@float', '')}
+                        width={1920}
+                        height={1080}
+                        alt={props.alt ? props.alt : 'no description provided'}
+                        className='md:max-w-md aspect-video'
+                    />
+                </div>
+            ) : null
+        },
+        p(props) {
+            return (
+                <div>{props.children}</div>
+            )
+        },
+        a(props) {
+            return (
+                <a
+                    href={props.href}
+                    className='hover:text-green-500 text-white w-fit p-1 underline hover:no-underline'
+                >{props.children}</a>
+            )
+        }
+    }
     return (
         <div className='grow min-h-fit flex flex-col border-2 border-gray-500' data-color-mode={theme} >
-            {/* <MDEditor
-                value={value}
-                preview='preview'
-                hideToolbar
-                //height="100%"
-                visibleDragbar={false}
-                className='grow'
-            //height='100%'
-            /> */}
             <Markdown
-                remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+                className='break-words'
+                remarkPlugins={[[remarkGfm, { singleTilde: false }], [remarkUnwrapImages]]}
                 rehypePlugins={[rehypeRaw]}
-                components={{
-                    code(props) {
-                        const { children, className } = props
-                        const match = /language-(\w+)/.exec(className || '')
-                        return match ? (
-                            <div className='flex flex-col'>
-                                <SyntaxHighlighter
-                                    className='w-fit self-center'
-                                    wrapLongLines
-                                    PreTag="div"
-                                    language={match[1]}
-                                    style={theme === 'dark' ? darkTheme : lightTheme}
-                                >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
-                            </div>
-                        ) : (
-                            <div className='flex flex-col'>
-                                <code className={`${className} w-fit self-center`}>
-                                    {children}
-                                </code>
-                            </div>
-                        )
-                    }
-                }}
-            >{value}</Markdown>
+                components={components}
+            >{markdown}</Markdown>
         </div>
     )
 }
