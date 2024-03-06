8c9e3d3f17ebb6fa8983143ec013a521ed48265e
Mohd Fardeen <frad.fardeen@gmail.com>
2024-03-06 06:38:04 +0000

    update nextJs config for SSR

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     basePath: '/portfolio-site',
     images: {
-        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'images.pexels.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
             }
         ]
     }
