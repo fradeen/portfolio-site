27ceb33fb8f134c3151120d16611cd0474fe8863
Mohd Fardeen <frad.fardeen@gmail.com>
2024-04-09 17:01:46 +0000

    Revert "Merge branch 'ssr_dev' into pages_dev"
    This reverts commit 26ffe8dc3b5c039af8ccd63c844c655ec66f696d, reversing
    changes made to 2c2adb57f1f4adc08758115488aeb1ae61ca6e5e.

--- src/app/portfolio/[categoryId]/page.tsx
+++ src/app/portfolio/[categoryId]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PreViewArticle from '@/components/previewArticle/preViewArticle'
 import prisma from '@/lib/db'
-import Link from 'next/link'
 export async function generateStaticParams() {
     const categories = await prisma.category.findMany({
         select: {
@@ -31,9 +30,6 @@ export default async function page({ params: { categoryId } }: { params: { categ
                         <PreViewArticle article={article} key={article.id} />
                     )
                 })}
-                <div className='self-center my-2'>
-                    <Link href={{ pathname: '/article/add', query: { categoryId: category.id, type: 'project' } }} className='bg-green-500 text-white text-2xl hover:font-semibold w-fit p-1 rounded-lg mb-5 self-center'>Add Project</Link>
-                </div>
             </div>
         </div>
     )
