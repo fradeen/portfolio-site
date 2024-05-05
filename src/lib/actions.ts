6d5ffe63fcba42e3be19bab58f70a67c748444b8
Mohd Fardeen <frad.fardeen@gmail.com>
2024-05-05 11:30:12 +0000

    update action for adding/updating user info.

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,22 +3,28 @@
 import { User } from "@prisma/client";
 import prisma from "./db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function updateUserInfo(user: User) {
     try {
 
-        let batchResult = await prisma.user.updateMany({
-            data: {
-                name: user.name,
-                home: user.home,
-                avatarSrc: user.avatarSrc,
-                title: user.title,
-                tags: user.tags,
-                intro: user.intro,
-                about: user.about
-            }
-        })
-        if (batchResult.count === 0)
+        const userId = await prisma.user.findFirst({ select: { id: true } })
+        if (userId)
+            await prisma.user.update({
+                where: {
+                    id: userId.id
+                },
+                data: {
+                    name: user.name,
+                    home: user.home,
+                    avatarSrc: user.avatarSrc,
+                    title: user.title,
+                    tags: user.tags,
+                    intro: user.intro,
+                    about: user.about
+                }
+            })
+        else
             await prisma.user.create({
                 data: {
                     name: user.name,
@@ -30,9 +36,9 @@ export async function updateUserInfo(user: User) {
                     about: user.about
                 }
             })
-        revalidatePath('/', 'layout')
-
     } catch (err) {
-
+        console.log(err)
     }
+    revalidatePath('/', 'layout')
+    redirect('/')
 }
