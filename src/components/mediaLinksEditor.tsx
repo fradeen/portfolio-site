f95ac742ec1fe0b50f11e8005cda700736c87d37
Mohd Fardeen <frad.fardeen@gmail.com>
2024-05-07 11:21:29 +0000

    WIP: add dashboard for updating/adding site info

--- src/components/mediaLinksEditor.tsx
+++ src/components/mediaLinksEditor.tsx
@@ -1,7 +1,10 @@
+'use client'
 import { SocialMediaLink } from '@prisma/client'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { cloudinaryUnoptimizedLoader } from '@/lib/imgLoader'
+import { addMediaLink } from '@/lib/actions'
+import MediaLinkForm from './mediaLinkForm'
 
 export default function MediaLinksEditor({ socialMediaLinks }: { socialMediaLinks: SocialMediaLink[] }) {
     return (
@@ -10,25 +13,21 @@ export default function MediaLinksEditor({ socialMediaLinks }: { socialMediaLink
             <div className='flex flex-wrap gap-3'>
                 {socialMediaLinks.map(link => {
                     return (
-                        <div key={link.id} className={`group max-w-xs p-3 rounded-lg border-2 flex gap-2 items-center hover:bg-gradient-to-l hover:from-${link.accentColor}`}>
-                            <div className='w-fit h-fit p-1 rounded-full bg-gray-100' aria-hidden>
-                                <Image src={link.imgSrc} width={32} height={32} alt={link.title} loader={cloudinaryUnoptimizedLoader} aria-hidden />
+                        <div key={link.id} className='group'>
+                            <div key={link.id} className={`group-hover:hidden group-focus-within:hidden max-w-xs p-3 rounded-lg border-2 flex gap-2 items-center hover:bg-gradient-to-l hover:from-${link.accentColor}`}>
+                                <div className='w-fit h-fit p-1 rounded-full bg-gray-100' aria-hidden>
+                                    <Image src={link.imgSrc} width={32} height={32} alt={link.title} loader={cloudinaryUnoptimizedLoader} aria-hidden />
+                                </div>
+                                <p className='text-2xl font-light' >{link.message}</p>
                             </div>
-                            <p className='text-2xl font-light group-hover:font-normal' >{link.message}</p>
+                            <MediaLinkForm link={link} />
                         </div>
                     )
                 })}
-                <div tabIndex={0} className='group border-2 p-3 w-fit max-w-screen-sm flex flex-col justify-center items-center'>
-                    <span className='w-full p-1'>Add</span>
-                    <div className='group-focus-within:grid grid grid-cols-3 auto-rows-auto'>
-                        <input name='title' type='text' required placeholder='Title*' className='border-2 p-2 max-w-xs rounded-lg focus:border-3 dark:bg-black dark:text-white h-fit' />
-                        <input name='url' type='text' required placeholder='URL*' className='border-2 p-2 max-w-xs rounded-lg focus:border-3 dark:bg-black dark:text-white h-fit' />
-                        <input name='imgSrc' type='text' required placeholder='Image Source*' className='border-2 p-2 max-w-xs rounded-lg focus:border-3 dark:bg-black dark:text-white h-fit' />
-                        <input name='accentColor' type='text' required placeholder='Accent Color*' className='border-2 p-2 max-w-xs rounded-lg focus:border-3 dark:bg-black dark:text-white h-fit' />
-                        <input name='message' type='text' required placeholder='Message*' className='border-2 p-2 max-w-xs rounded-lg focus:border-3 dark:bg-black dark:text-white h-fit' />
-                        <button type='button' className='rounded-lg p-2 border-2 w-full hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white' onClick={() => { }}>Add</button>
-                    </div>
-                </div>
+            </div>
+            <div tabIndex={0} className='border-2 p-3 w-fit flex flex-col justify-center items-center'>
+                <span className='w-full p-1'>Add</span>
+                <MediaLinkForm />
             </div>
         </div>
     )
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { User } from "@prisma/client";
+import { Project, SocialMediaLink, User } from "@prisma/client";
 import prisma from "./db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -41,4 +41,61 @@ export async function updateUserInfo(user: User) {
     }
     revalidatePath('/', 'layout')
     redirect('/')
+}
+
+export async function addMediaLink(link: SocialMediaLink) {
+    try {
+        await prisma.socialMediaLink.create({
+            data: {
+                title: link.title,
+                url: link.url,
+                imgSrc: link.imgSrc,
+                accentColor: link.accentColor,
+                message: link.message
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    revalidatePath('/', 'layout')
+    //redirect('/')
+}
+
+export async function updateMediaLink(link: SocialMediaLink) {
+    try {
+        let result = await prisma.socialMediaLink.update({
+            where: {
+                id: link.id
+            },
+            data: {
+                title: link.title,
+                url: link.url,
+                imgSrc: link.imgSrc,
+                accentColor: link.accentColor,
+                message: link.message
+            }
+
+        })
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    }
+    revalidatePath('/', 'layout')
+    //redirect('/')
+}
+
+export async function addProject(project: Project) {
+    try {
+        await prisma.project.create({
+            data: {
+                title: project.title,
+                imgSrc: project.imgSrc,
+                markdown: project.markdown
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    revalidatePath('/', 'layout')
+    //redirect('/')
 }
