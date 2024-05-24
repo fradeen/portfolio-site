22700e0b3f6dc3ef909366d810dba0adb8bdf689
Mohd Fardeen <frad.fardeen@gmail.com>
2024-05-24 17:50:05 +0000

    Revert "rollback projects links; untill projects are added to DB."
    This reverts commit f6f44545972516ffae0346c24c78360f56d64dd2.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,26 @@
+import ProjectPreview from '@/components/projectPreview'
+import prisma from '@/lib/db'
+import Link from 'next/link'
+import React from 'react'
+
+export default async function Projects() {
+    const projects = await prisma.project.findMany({})
+    return (
+        <main className='customContainer flex-col justify-start items-center gap-10'>
+            <h1 className='w-full'>My Projects</h1>
+            <section aria-label='Project Links' className='flex flex-col gap-3 items-center justify-center grow'>
+                {projects && projects.length > 0 ?
+                    projects.map(project => (
+                        <Link key={project.id} href={`/projects/${project.id}`} aria-labelledby={`${project.id}-title`}>
+                            <ProjectPreview project={project} />
+                        </Link>
+                    ))
+                    : (
+                        <section>
+                            <h1>No projects found.</h1>
+                        </section>
+                    )}
+            </section>
+        </main>
+    )
+}
